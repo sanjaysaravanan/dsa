7c7403d0b4a63b914960850e607796a83bffbfa6
Sanjay Saravanan <sanjay.saravanan@encora.com>
2024-08-09 19:12:21 +0530

    added binary tree traversal

--- binary-tree/inorder-traversal.js
+++ binary-tree/inorder-traversal.js
@@ -5,4 +5,28 @@ function TreeNode(val, left, right) {
   this.right = right === undefined ? null : right;
 }
 
-const tree = new TreeNode(1, new TreeNode(2, null, new TreeNode(5)));
+const tree = new TreeNode(1);
+
+var arr = [];
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+const inOrderTravesal = (root, prefix = "") => {
+  if (!root) {
+    console.log(prefix);
+    return;
+  }
+
+  if (root.left || root.right) {
+    prefix = `${prefix}${root.val}->`;
+    root.left && inOrderTravesal(root.left, prefix);
+    root.right && inOrderTravesal(root.right, prefix);
+  } else {
+    prefix = `${prefix}${root.val}`;
+    arr.push(prefix);
+  }
+  return arr;
+};
+
+console.log(inOrderTravesal(tree));
